4e0ec54e7d3690394379b9c67373ee5c129816b7
khc <khc@tesollo.com>
2026-03-19 17:35:24 +0900

    fix: Remove missing logo references from Sphinx config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,8 +34,6 @@ html_theme = "furo"
 html_static_path = ["_static"]
 html_title = "Delto Gripper ROS 2"
 html_theme_options = {
-    "light_logo": "logo-light.png",
-    "dark_logo": "logo-dark.png",
     "sidebar_hide_name": False,
     "navigation_with_keys": True,
     "footer_icons": [
